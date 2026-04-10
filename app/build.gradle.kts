bdbd7b5775363efd480b93a029fcdcc64dc774f0
aidil24si <aidil24si@mahasiswa.pcr.ac.id>
2026-04-10 08:23:26 +0700

    Pnerapan View Binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+ plugins {
     alias(libs.plugins.android.application)
 }
 
@@ -30,6 +30,10 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
 }
 
---
+++ app/src/main/java/com/example/greenlite/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,37 @@
+package com.example.greenlite.pertemuan_3
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.greenlite.R
+import com.example.greenlite.R.id.inputNoTujuan
+import com.example.greenlite.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnKirim.setOnClickListener {
+            val nomor = binding.inputNoTujuan.text
+            Log.e("Klik btnSubmit", "Tombol berhasil di tekan. Isi dari inputNama = $nomor")
+
+            Toast.makeText(this, "Pesan berhasil dikirim $nomor", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+}
